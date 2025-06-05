7f9e10d71f30672425a1f861363181c4818f682d
! LO$R <106779656+madhead341@users.noreply.github.com>
2025-06-05 20:51:02 +0200

    Update install_requirements.bat

--- install_requirements.bat
+++ install_requirements.bat
@@ -1,7 +1,7 @@
 @echo off
 echo Installing required Python packages...
 python -m pip install --upgrade pip
-pip install -r files/requirements.txt
+pip install -r RAT/files/requirements.txt
 
 echo.
 echo Installation complete.
